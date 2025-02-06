4bc09ec28fbeced449e41cdc51f21c7b5a8dd43e
Sakura <sakurathetrans@gmail.com>
2025-02-06 13:32:58 -0500

    COMPACT Pt 2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 // 34 Lines -> 11 Lines
 public class Main {
     static double[] tempsList = {0.0,9.3,-2.4,8.3,7.7,16.8,14.1,26.3};
-    static void calcF(double celHigh, double celLow) {
-        System.out.println("The low will be " + (9.0/5*celHigh+32) + " and the high will be " + (9.0/5*celLow+32) + ".");
-    }
     public static void main(String[] args) {
         for (int i = 0; i < tempsList.length; i += 2) {
-            calcF(tempsList[i],tempsList[i+1]);
+            System.out.println("The low will be " + (9.0/5*(tempsList[i])+32) + " and the high will be " + (9.0/5*(tempsList[i+1])+32) + ".");
         }
     }
 }
